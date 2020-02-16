ed61e5d04a811f181d6770eedc034f343bdaedf5
abdulrahman118 <developer.abdulrahman@gmail.com>
2020-02-17 02:13:15 +0530

    Updates

---
+++ precache-manifest.526703dae6102e4d4fcf58c0d06ebcf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed26715c7c9cb6cdaf5ead95a530c458",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "1849c09ffd0d6f69f289",
+    "url": "/sudoku/static/css/main.60db8046.chunk.css"
+  },
+  {
+    "revision": "f673acc1d4409c89dbea",
+    "url": "/sudoku/static/js/2.0938546d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sudoku/static/js/2.0938546d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1849c09ffd0d6f69f289",
+    "url": "/sudoku/static/js/main.982fdacc.chunk.js"
+  },
+  {
+    "revision": "80fdc32de6d895578ae2",
+    "url": "/sudoku/static/js/runtime-main.c8e770ab.js"
+  }
+]);
